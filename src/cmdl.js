5ca53c7baa976f4254441b47fb71887dc9d39eb3
Abner Doubleday <abnerdoubleday@MacBook-Air.localdomain>
2022-10-27 18:34:21 -0700

    Added command line args structure.

--- src/cmdl.js
+++ src/cmdl.js
@@ -1,64 +1,70 @@
 const cLog = require('./constants/structuredLogger').toConsole;
 
-/* BARCODE CREATOR COMMAND LINE APPLICATION
-
-* This is the cmd entry point into the application.
-* Use this module to create barcodes from the command
-* line.
-*
-* Command: generate-barcode
-*   generates a one off barcode svg with barcode data as
-*   the filename.
-*
-* Options:
-*   data (required): the data to be encoded into the barcode
-*
-*   size: 
-*    the width and height of the barcode to barcodes
-*    generated.  Formatted as "w:h". By default This 
-*    value is autocalculated.
-*   
-*   showdata (defaulted): 
-*    shows the encoded data in ascii characters.
-*   
-*   fontsize (defaulted): 
-*    size of the font.  By default this is auto calculated
-* 
-*   margin (defaulted):
-*    amount of space to be added around the barcode and data.
-*
-*   border:
-*    adds a border around the margin of the barcode
-*
-* Command: generate-barcodes
-*   generates serveral barcode svgs from a file of line
-*   separated barcodes.
-*
-* Options:
-*   file (required): file of line separated barcodes.  A 
-*    check will be performed against the file to ensure 
-*    that barcodes are in compliance
-*
-*   size: 
-*    the width and height of the barcode to barcodes
-*    generated.  Formatted as "w:h". By default This 
-*    value is autocalculated.
-*   
-*   showdata (defaulted): 
-*    shows the encoded data in ascii characters.
-*   
-*   fontsize (defaulted): 
-*    size of the font.  By default this is auto calculated
-* 
-*   margin (defaulted):
-*    amount of space to be added around the barcode and data.
-*
-*   border:
-*    adds a border around the margin of the barcode
-*
-*   printsize:
-*    creates a page jpeg compatible with a particular printable
-*    page size "w:h" in inches.
-*/
-
+yargs
+	.command(
+		'barcode',
+		'generate an image of a single barcode',
+		function(yargs) {
+			return yargs
+				.option('data', {
+					alias: 'd',
+					describe: 'the data to be encoding into a barcode'
+				})
+				.option('addlabel', {
+					alias: 'l',
+					describe: 'add the data label to the barcode image'
+				})
+				.option('size', {
+					alias: 's',
+					describe: 'size of the barcode image "(w:h)"'
+				})
+				.option('addborder', {
+					alias: 'b',
+					describe: ''
+				})
+				.option('margin', {
+					alias: 'm',
+					describe: 'margin around the barcode inside the border.'
+				})
+		},
+		function(argv) {
+			console.log(argv)
+		}
+	)
+	.command(
+		'barcodes',
+		'generate a page of barcode images',
+		function(yargs) {
+			return yargs
+				.option('datafile', {
+					alias: 'd',
+					describe: 'the file with line separated data to be encoding into a barcode'
+				})
+				.option('addlabel', {
+					alias: 'l',
+					describe: 'add the data label to the barcode image'
+				})
+				.option('size', {
+					alias: 's',
+					describe: 'size of the barcode image "(w:h)"'
+				})
+				.option('addborder', {
+					alias: 'b',
+					describe: ''
+				})
+				.option('margin', {
+					alias: 'm',
+					describe: 'margin around the barcode inside the border'
+				})
+				.option('pagesize', {
+					alias: 'p',
+					describe: 'size for page for paged barcodes'
+				})
+		},
+		function(argv) {
+			console.log(argv)
+		}
+	)
+	.help()
+	.argv
 
